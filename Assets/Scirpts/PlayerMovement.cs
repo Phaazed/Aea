f27d4cf360db7b3945ab4826ccd80da318731570
ensembleofwar <74737135+ensembleofwar@users.noreply.github.com>
2020-11-19 15:05:50 -0500

    Scripts

---
+++ Assets/Scirpts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float speed = 5f;
+
+    public Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float LeftRight = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(LeftRight, 0);
+        transform.Translate(movement * Time.deltaTime * speed, 0);
+
+        Vector2 characterScale = transform.localScale;
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -1;
+        }
+        else if(Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 1;
+        }
+        transform.localScale = characterScale; 
+    } 
+}
